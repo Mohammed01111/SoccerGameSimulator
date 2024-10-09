4a51eeca32f8c79e1b57513ce29937affc71a272
Mohammed <Mohammed.almanwari.01@gmail.com>
2024-10-09 14:18:25 +0400

    Player Constructor

---
+++ SoccerGameSimulator/Player.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoccerGameSimulator
+{
+    public class Player
+    {
+        public string Name { get; set; }
+        public string Position { get; set; }
+        public int Skill { get; set; }
+
+        public Player(string name, string position, int skill)
+        {
+            Name = name;
+            Position = position;
+            Skill = skill;
+        }
+    }
+}

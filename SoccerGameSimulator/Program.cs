0f066247c6ff261279e01c959ca13469a58ddc25
Mohammed <Mohammed.almanwari.01@gmail.com>
2024-10-09 15:48:53 +0400

    modify main function

--- SoccerGameSimulator/Program.cs
+++ SoccerGameSimulator/Program.cs
@@ -4,7 +4,14 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Welcome to the Soccer Game Simulator!");
+            Console.WriteLine("Enter name for Team 1:");
+            string team1Name = Console.ReadLine();
+            Console.WriteLine("Enter name for Team 2:");
+            string team2Name = Console.ReadLine();
+
+            SoccerGame game = new SoccerGame(team1Name, team2Name);
+            game.StartGame();
         }
     }
 }

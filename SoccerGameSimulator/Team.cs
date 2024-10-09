ea2c75500526dc0d86ed3560f2fd0d9067341764
Mohammed <Mohammed.almanwari.01@gmail.com>
2024-10-09 16:43:40 +0400

    modify team class

--- SoccerGameSimulator/Team.cs
+++ SoccerGameSimulator/Team.cs
@@ -23,11 +23,15 @@ namespace SoccerGameSimulator
         {
             Random rand = new Random();
 
+            // Shuffle the positions randomly
+            List<string> shuffledPositions = positions.OrderBy(x => rand.Next()).ToList();
+
             for (int i = 0; i < 11; i++)
             {
                 string name = playerNames[i]; // Choose a name from the given list
-                string position = positions[i % 4]; // Alternate between positions 
+                string position = shuffledPositions[i % shuffledPositions.Count]; // Randomly pick a position
                 int skill = rand.Next(50, 101);  // Skill between 50 and 100
+
                 Player player = new Player(name, position, skill);
                 Players.Add(player);
             }

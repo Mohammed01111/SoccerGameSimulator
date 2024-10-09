e9572bd2ab6ff3b24c1b8480869f8c0f75cad009
Mohammed <Mohammed.almanwari.01@gmail.com>
2024-10-09 14:26:19 +0400

    add Team class with Constructor

---
+++ SoccerGameSimulator/Team.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoccerGameSimulator
+{
+    public class Team
+    {
+        public string Name { get; set; }
+        public List<Player> Players { get; private set; }
+        public int Score { get; set; }
+
+        public Team(string name)
+        {
+            Name = name;
+            Players = new List<Player>();
+            Score = 0;
+        }
+    }
+}

77788b58256a24939775bcbbca2f16a201e52dde
Mohammed <Mohammed.almanwari.01@gmail.com>
2024-10-09 15:07:59 +0400

    add SoccerGame class and its  Constructor

---
+++ SoccerGameSimulator/SoccerGame.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoccerGameSimulator
+{
+    public class SoccerGame
+    {
+        private Team team1;
+        private Team team2;
+        private Random rand;
+
+        public SoccerGame(string team1Name, string team2Name)
+        {
+            team1 = new Team(team1Name);
+            team2 = new Team(team2Name);
+            rand = new Random();
+        }
+    }
+}
